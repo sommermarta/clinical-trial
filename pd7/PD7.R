bbced79ef773be58be897549e490732edf541772
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2014-12-01 23:10:50 +0100

    Marcin wrzuca 7me pd zad 1c i tabelki

--- pd7/PD7.R
+++ pd7/PD7.R
@@ -19,22 +19,33 @@ n <- nNormal(delta1=5,sd=17,alpha=0.05,beta=0.1,sided=2)
 (x <- gsDesign(k=3,test.type=2,alpha=0.05,n.fix=ceiling(n),beta=0.1,sfu='WT',sfupar=0.2))
 plot(x)
 
-theta <- seq(0,2,by=0.5)
+
 x$upper$bound[2]
 x$n.I[2]
 
+
+theta <- seq(0,0.5,by=0.01)
 delta <- sapply(theta, function(theta){
-  x <- gsDesign(k=3,test.type=2,alpha=0.05,
-                n.fix=ceiling(n),beta=0.1,sfu='WT',sfupar=theta)
-  x$upper$bound[2]*sqrt(4)*17/sqrt(x$n.I[2])
+   x <- gsDesign(k=3,test.type=2,alpha=0.05,
+                 n.fix=ceiling(n),beta=0.1,sfu='WT',sfupar=theta)
+   x$upper$bound[2]*sqrt(2)*34/sqrt(x$n.I[2])
 })
+wykres <- data.frame(theta = theta, delta = delta)
+library(ggplot2)
+library(ggthemes)
+ggplot(wykres, aes(x = theta, y = delta)) + geom_smooth(lwd=2)+
+   geom_hline(yintercept=5, col = "orange", lwd=2)+
+   geom_vline(xintercept=0.4, col = "red", lwd=2)+
+   theme_bw(base_family="serif", base_size=20)
+
 
 plot(theta, delta,type='l')
+abline(a=5,b=0, "red")
 theta[which(delta>5)]
 # theta =2.5
 
 gsDesign(k=3,test.type=2,alpha=0.05,
-         n.fix=ceiling(n),beta=0.1,sfu='WT',sfupar=1)
+         n.fix=ceiling(n),beta=0.1,sfu='WT',sfupar=0.4)
 
 
 ### ZAD 2
--- pd7/tabelkaDlaAdama.R
+++ pd7/tabelkaDlaAdama.R
@@ -1,10 +1,12 @@
 
 x <- gsDesign(k=3,test.type=2,alpha=0.05,
               n.fix=ceiling(n),beta=0.1,sfu='WT',sfupar=0.4)
-y <- cbind(x$n.I,x[19]$upper$bound, x[19]$upper$prob)
+y <- cbind(x$n.I,x$upper$bound, 1-pnorm(x$upper$bound), x$upper$spend )
 
 y <- as.data.frame(y)
 names(y) <- c("n", "Z", "Nominal p", "spend")
 
 library(xtable)
-xtable(y)
+xtable(y, digits=3)
+
+
